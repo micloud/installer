9d29fafc378cb066a147f6daff144d98d529105c
simonsu <simonsu@mitac.com.tw>
2013-03-24 08:50:28 +0800

    add clean process

--- smartos-standard64/install-nodejs.sh
+++ smartos-standard64/install-nodejs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+export LOG=/root/init.log
 log( ){
-  echo $1 >> /root/init.log
+  echo $1 >> $LOG
 }
 
-cd /opt
-wget http://nodejs.org/dist/v0.10.1/node-v0.10.1.tar.gz
-tar -xzf node-v0.10.1.tar.gz
+rm $LOG
+rm /opt/node*
+
+env >> $LOG
+
 log 'Update database...' 
 pkgin update
 
@@ -14,10 +17,16 @@ pkgin -y install `pkgin se gcc | grep ^gcc | awk '{print $1}'`
 pkgin -y install `pkgin search gmake | grep ^gmake | awk '{print $1}'`
 pkgin -y install `pkgin se python | grep ^python | grep 2.6 | awk '{print $1}'`
 
+log 'Downloading node.js'
+cd /opt
+wget http://nodejs.org/dist/v0.10.1/node-v0.10.1.tar.gz
+tar -xzf node-v0.10.1.tar.gz
+
 log 'Compile resources...'
 cd /opt/node-v0.10.1
 mkdir /opt/bin
-./configure --prefix=/opt/bin/node-v0.10.1 && make && make install
+/opt/bin
+cd /opt/node-v0.10.1/configure --prefix=/opt/bin/node-v0.10.1 && make && make install
 
 cd /opt
 log 'Making symbolic link...'
